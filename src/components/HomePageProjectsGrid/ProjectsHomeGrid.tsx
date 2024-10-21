9d465655490b5d6f77b3d0a8fe03ab34ab471ed1
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-10-21 23:00:00 +0530

    feat: minor updates

--- src/components/HomePageProjectsGrid/ProjectsHomeGrid.tsx
+++ src/components/HomePageProjectsGrid/ProjectsHomeGrid.tsx
@@ -2,7 +2,7 @@ import GridItem from "./GridItem"
 
 export default function ProjectsHomeGrid() {
   return (
-    <div className="container bg-black mx-auto px-4 py-8 -mt-48 relative z-10">
+    <div className="container bg-black mx-auto px-4 py-8 -mt-32 md:-mt-48 relative z-10">
       <div className="flex flex-wrap -mx-4">
         <GridItem />        
         <GridItem />        
