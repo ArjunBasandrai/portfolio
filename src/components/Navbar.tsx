7f33ca630376667e6b103ac2e1c86f8b6b4ef9ea
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-10-22 17:54:16 +0530

    feat: updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,43 @@
 'use client';
 
-import { useState } from 'react';
 import Link from 'next/link';
-// import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+    const links = ['Home', 'Projects', 'About'];
+    const pathname = usePathname();
 
     return (
-        <nav className="fixed top-4 inset-x-0 z-50 bg-gray-800 text-white rounded-[50px] mx-auto w-max px-6">
-            <div className="inline-flex items-center justify-center p-4">
-                <div className='pr-10'>
-                    <Link href="/">Home</Link>
-                </div>
-                <div className='pr-10'>
-                    <Link href="/projects">Projects</Link>
-                </div>
-                <div className=''>
-                    <Link href="/about">About</Link>
-                </div>
+        <nav className="fixed top-8 inset-x-0 z-50 bg-gray-900 text-white rounded-[50px] mx-auto w-max px-2 border-[0.2px] border-gray-700">
+            <div className="inline-flex items-center px-1 py-[15px] justify-between w-[250px]">
+                {links.map((link, index) => {
+                    const href = `/${link === 'Home' ? '' :link.toLowerCase()}`;
+                    const isActive = pathname === href || (pathname === '/' && link === 'Home');
+
+                    const isFirst = index === 0;
+                    const isLast = index === links.length - 1;
+
+                    let additionalClasses = '';
+                    if (isActive) {
+                        if (isFirst) {
+                            additionalClasses = 'rounded-l-[50px]';
+                        } else if (isLast) {
+                            additionalClasses = 'rounded-r-[50px]';
+                        }
+                    }
+
+                    return (
+                        <div key={index}>
+                            <Link
+                                href={href}
+                                className={`px-3 py-2 rounded-md ${isActive ? `bg-gray-700 ${additionalClasses}` : ''}`}
+                            >
+                                {link}
+                            </Link>
+                        </div>
+                    );
+                })}
             </div>
         </nav>
-
     );
 }
