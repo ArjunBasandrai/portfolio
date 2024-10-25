e39fc1ea2e4a818f1d548aee2a1665e116b6e029
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-10-25 13:13:20 +0530

    feat: fetch posts from Featured series

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,43 +1,10 @@
 import '../../styles/globals.css';
 import ProjectsPage from "@/pages/ProjectsPage";
 
-import { gql } from "@apollo/client";
-import client from "../../lib/apollo-client"
-
-export default async function Projects() {
-    const { data } = await client.query({
-      query: gql`
-        query {
-          publication(id: "6717ca18fd2be89bc676fc81") {
-            series(slug: "featured") {
-              id
-              name
-              posts(first: 20) {
-                edges {
-                  node {
-                    id
-                    title
-                    subtitle
-                    content {
-                      html
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      `,
-    });
-  
-    const series = data.publication.series;
-    
-    console.log(series.posts.edges[0].node);
-  
+export default function Projects() {
+      
     return (
-      <div className="mt-[100px]">
         <ProjectsPage />
-      </div>
     );
   }
   
