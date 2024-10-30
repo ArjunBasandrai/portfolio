e4ea30ae1635909b33fcaf0804c5c1f9b7c06221
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-10-30 23:35:55 +0530

    chore: hashnode cache patch

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -49,6 +49,7 @@ export default function ProjectPage({ projectSlug }: {
                                     html
                                 }
                                 previousSlugs
+                                cuid
                             }
                         }
                     }
