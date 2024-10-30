75407a1252c1ac651144e0055304f00cdf4869d6
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-10-30 23:08:12 +0530

    feat: minor style changes for tech stack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 export default function TechStack({ stack }: { stack: string[] }) {
     return (
         <>
-            <div className="flex flex-wrap gap-4 justify-center rounded-lg px-0 md:px-2 py-6 sm:gap-6 md:gap-8">
+            <div className="flex flex-wrap gap-4 justify-center rounded-lg px-0 md:px-2 py-6 sm:gap-6">
                 {stack.map((tech, index) => (
                     <IconHandler key={index} name={tech} />
                 ))}
