1417cbaeaf3de2f8005fd6166d66a6291c776b42
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-10-31 10:24:53 +0530

    fix: add favicon to public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar"
 export const metadata = {
   title: 'Arjun Basandrai - Software Engineer',
   description: 'Arjun Basandrai is a Software Engineer based in Punjab, India. He is passionate about working with large datasets to create real world applications and in his free time he loves to photograph the wildlife around him',
+  icons: [{ rel: "icon", url: "/favicon.ico"}],
 }
 
 export default function RootLayout({
