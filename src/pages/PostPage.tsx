b9e1e963f61221570d90958e2f1916cee2e8a209
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2025-01-16 22:31:03 +0530

    chore: minor changes

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -49,8 +49,7 @@ export default function ProjectPage({ projectSlug }: {
                                 content {
                                     html
                                 }
-                                brief
-                                slug
+                                url
                             }
                         }
                     }
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -32,7 +32,7 @@ export default function ProjectsPage() {
                                         seo {
                                             description
                                         }
-                                        previousSlugs
+                                        url
                                     }
                                 }
                             }
